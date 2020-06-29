5c4e5c70713805109442a5aa30849505c7f3aa1e
Diego <mr.dieonni@gmail.com>
2020-06-29 10:48:09 +0200

    abbellimento

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());  
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 
 
 app.get('/', (req, res) => {

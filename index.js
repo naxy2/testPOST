98b96973131a53a1a8411e6c0206cf74c08dcbec
Diego <mr.dieonni@gmail.com>
2020-07-02 23:13:48 +0200

    sistemato errore

--- index.js
+++ index.js
@@ -11,13 +11,9 @@ const port = process.env.PORT;
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
+    // Pass to next layer of middleware
     next();
 });
 

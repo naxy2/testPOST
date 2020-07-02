902c3da465e74ea0e56727dae554507b6f0e36f0
Diego <mr.dieonni@gmail.com>
2020-07-02 23:08:21 +0200

    aggiunto header cross

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const file = "./data.json"
 const app = express();
 const port = process.env.PORT;
 
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    next();
+});
+
+
 app.use(express.json());
 
 var data = {messages: []} 
